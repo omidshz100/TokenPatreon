f83098d9c2f8660f9c4d4aa75945631aa4b4fb10
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-06-21 12:51:22 +0200

    Initial Commit

---
+++ TokenPatreon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TokenPatreon
+//
+//  Created by Omid Shojaeian Zanjani on 21/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TokenPatreon/TokenPatreonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenPatreonApp.swift
+//  TokenPatreon
+//
+//  Created by Omid Shojaeian Zanjani on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TokenPatreonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
